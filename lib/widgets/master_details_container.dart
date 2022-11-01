7f970ce9bb301d12b26669aa8a38fc0fe2ef336b
marionegroponzi <marionegroponzi@gmail.com>
2022-11-01 14:36:34 +0100

    Moved data loading to data model

--- lib/widgets/master_details_container.dart
+++ lib/widgets/master_details_container.dart
@@ -7,7 +7,6 @@ import 'package:scrptr/models/data_model.dart';
 import 'package:scrptr/models/item.dart';
 import 'package:scrptr/widgets/item_details.dart';
 import 'package:scrptr/widgets/item_listing.dart';
-import 'package:yaml/yaml.dart';
 
 class MasterDetailContainer extends StatefulWidget {
   const MasterDetailContainer({Key? key}) : super(key: key);
@@ -50,22 +49,10 @@ class _ItemMasterDetailContainerState extends State<MasterDetailContainer> {
   }
 
   _onLoad(context, button) async {
-    _stdoutController.text = "";
-    _stderrController.text = "";
-    debugPrint("_onLoad");
     FilePickerResult? result = await FilePicker.platform.pickFiles();
     if (result != null) {
-      final str = result.files.single.path!;
-      debugPrint(str);
-      File file = File(str);
-      final data = await file.readAsString();
-      final mapData = loadYaml(data) as Map;
-      // debugPrint(mapData.toString());
-      final l = mapData["commands"] as List;
-      final itemList = l.map((e) => Item(title: e["title"], command: e["command"])).toList();
-      debugPrint(itemList.toString());
-      DataModel.of(context)?.items.clear();
-      DataModel.of(context)?.items.addAll(itemList);
+      final fileName = result.files.single.path!;
+      await DataModel.of(context)?.loadFile(fileName);
       setState(() {
         _stdoutController.text = "";
         _stderrController.text = "";
@@ -84,8 +71,8 @@ class _ItemMasterDetailContainerState extends State<MasterDetailContainer> {
           child: Material(
             elevation: 4.0,
             child: ItemListing(
-              itemSelectedCallback: _onSelectionChanged,
-              loadButtonPressed: _onLoad,
+              onSelectionChanged: _onSelectionChanged,
+              onLoadButtonPressed: _onLoad,
               selectedItem: _selectedItem,
             ),
           ),
