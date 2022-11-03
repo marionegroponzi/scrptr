f08ab17f59a37f9cd0c8f3e7ad27a002902e1556
Mario Negro Ponzi <mario.negro.ponzi@nl.abnamro.com>
2022-11-03 16:37:57 +0100

    Parameters now show up correctly

--- lib/widgets/item_details.dart
+++ lib/widgets/item_details.dart
@@ -2,19 +2,19 @@
 
 // import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+
 // import 'package:path_provider/path_provider.dart';
 import 'package:scrptr/models/item.dart';
 import 'package:scrptr/app_widgets/app_button.dart';
 import 'package:scrptr/app_widgets/app_textarea.dart';
 
 class ItemDetails extends StatefulWidget {
-  const ItemDetails(
-      {Key? key,
-      required this.isInTabletLayout,
-      required this.item,
-      required this.runButtonPressed,
-      required this.stdoutController,
-      required this.stderrController})
+  const ItemDetails({Key? key,
+    required this.isInTabletLayout,
+    required this.item,
+    required this.runButtonPressed,
+    required this.stdoutController,
+    required this.stderrController,})
       : super(key: key);
 
   final bool isInTabletLayout;
@@ -28,53 +28,127 @@ class ItemDetails extends StatefulWidget {
 }
 
 class _ItemDetailsState extends State<ItemDetails> {
+  String getParameter(int index) {
+    final item = widget.item;
+    if (item != null && item.parameters != null && index < item.parameters!.length) {
+      return item.parameters![index];
+    }
+    return "";
+  }
+
+  getLength() {
+    final item = widget.item;
+    if (item != null && item.parameters != null) {
+      return item.parameters!.length;
+    }
+    return 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final Widget content = Column(
+      final Widget content = Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Flexible(
-          // child: Flex(
-          //   children: [Text("test")],
-          //   direction: Axis.vertical,
-          // ),
+          flex: 1,
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Text(
-                      widget.item?.command ?? 'Please select one command on the left.',
-                      style: const TextStyle(
-                        fontFamily: 'RobotoMono',
-                        fontSize: 16,
+            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        widget.item?.command ??
+                            'Please select one command on the left.',
+                        style: const TextStyle(
+                          fontFamily: 'RobotoMono',
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.start,
                       ),
-                      textAlign: TextAlign.start,
-                    ),
+                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
         Flexible(
+          flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ListView.builder(
+              itemCount: getLength(),
+              prototypeItem: const ListTile(
+                title: Text(""),
+              ),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Text("${getParameter(index)}: "),
+                  title: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (text) {
+                      print('First text field: $text');
+                    },
+                  )
+                );
+              },
+            ),
+          ),
+        ),
+        Flexible(
+          flex: 2,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Flexible(child: AppTextArea(borderColor: Colors.grey, controller: widget.stdoutController)),
               Flexible(
-                  child: AppTextArea(
-                borderColor: Colors.red,
-                controller: widget.stderrController,
-              )),
+                flex: 1,
+                child: Column(
+                  children: [
+                    const Flexible(
+                      flex: 1,
+                      child: Text("Standard Output"),
+                    ),
+                    Flexible(
+                      flex: 10,
+                      child: AppTextArea(
+                                borderColor: Colors.grey,
+                                controller: widget.stdoutController,
+                            ),
+                    )
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: [
+                    const Flexible(
+                      flex: 1,
+                      child: Text("Standard Error"),
+                    ),
+                    Flexible(
+                      flex: 10,
+                      child: AppTextArea(
+                        borderColor: Colors.red,
+                        controller: widget.stderrController,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
         Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          AppButton(buttonPressed: widget.runButtonPressed, buttonTitle: "Run", disabled: widget.item == null),
+          AppButton(
+              buttonPressed: widget.runButtonPressed,
+              buttonTitle: "Run",
+              disabled: widget.item == null),
         ])
       ],
     );
@@ -87,4 +161,6 @@ class _ItemDetailsState extends State<ItemDetails> {
       body: Center(child: content),
     );
   }
+
+
 }
